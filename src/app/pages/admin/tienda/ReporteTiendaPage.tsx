d5dde875fe17b32ba5d2ff19570b660f758b25bf
alonsodelcordova <alonsodelcordova@gmail.com>
2024-06-20 23:58:47 -0500

    add: ventas r

--- src/app/pages/admin/tienda/ReporteTiendaPage.tsx
+++ src/app/pages/admin/tienda/ReporteTiendaPage.tsx
@@ -1,8 +1,70 @@
+import { useState } from "react";
+import { strFormatDateBackend } from "../../../utils/formats";
+import { getReporteVentas } from "../../../services/ventas.service";
+import DatePicker from "react-datepicker";
 
 export default function ReporteTiendaPage() {
+
+    const [startDate, setStartDate] = useState<Date | null>(null);
+    const [endDate, setEndDate] = useState<Date | null>(null);
+    const [ url, setUrl ] = useState("")
+
+    const getReporte = ()=>{
+        let startFecha = strFormatDateBackend(startDate||new Date())
+        let endFecha = strFormatDateBackend(endDate||new Date())
+        
+        let url =  getReporteVentas(startFecha, endFecha)
+        setUrl(url)
+    }
+
     return(
-        <>
-            <h3>Reporte Tienda</h3>
-        </>
+        <div className="container-md my-3">
+
+            <div className="card card-body text-black my-3">
+                <h3>Reporte Tienda</h3>
+
+                <div className="row">
+                    <div className="col-md-6 my-2">
+                        <div className="form-group">
+                            <label>Fecha Inicio</label><br />
+                            <DatePicker
+                                selected={startDate}
+                                onChange={(date) => setStartDate(date)}
+                                showIcon
+                                className='form-control'
+                                placeholderText='dd/mm/yyyy'
+                                dateFormat="dd/MM/yyyy"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-md-6 my-2">
+                        <div className="form-group">
+                            <label>Fecha Fin</label> <br />
+                            <DatePicker
+                                selected={endDate}
+                                onChange={(date) => setEndDate(date)}
+                                showIcon
+                                className='form-control'
+                                placeholderText='dd/mm/yyyy'
+                                dateFormat="dd/MM/yyyy"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="form-group mt-2">
+                    <button className="btn btn-primary"
+                        onClick={getReporte}
+                    >Generar Reporte</button>
+                </div>
+
+            </div>
+
+            {url != ''  && 
+                <iframe src={url} width="100%" height="800px"/>
+            }
+
+
+        </div>
     )
 }
--- src/app/services/ventas.service.ts
+++ src/app/services/ventas.service.ts
@@ -1,5 +1,5 @@
 import { Venta } from "../types/Ventas";
-import { ApiResponse, getData, postData } from "../utils/main.service";
+import { ApiResponse, getData, getUrl, postData } from "../utils/main.service";
 
 
 
@@ -10,4 +10,8 @@ export async  function  getVentas(skip=0, limit=100): Promise<ApiResponse<Venta[
 
 export async function saveVenta(venta: Venta): Promise<ApiResponse<Venta>>{
     return await postData('ventas', venta);
+}
+
+export function getReporteVentas(star:string, end:string) {
+    return getUrl('ventas/reporte?fechaStart='+star+'&fechaEnd='+end)
 }
